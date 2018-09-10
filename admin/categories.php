cb1815ce30f7296a5ad317499640e43506bd11e7
Akash Dixit <anshudixit00011@gmail.com>
2018-09-10 22:02:02 +0530

    changes

--- admin/categories.php
+++ admin/categories.php
@@ -1,7 +1,8 @@
 <?php include "includes/adHeader.php"; ?>
 
     <div id="wrapper">
-             <!-- Navigation -->
+
+        <!-- Navigation -->
         <?php include "includes/adNavigation.php"; ?>
         <div id="page-wrapper">
 
@@ -14,6 +15,19 @@
                             Welcome To Admin
                             <small>Author</small>
                         </h1>
+
+                        <div class="col-xs-6">
+                            <form action=""  method="post">
+                                <div class="form-group">
+                                    <label for="cat-title">Add Category</label>
+                                    <input type="text" class="form-control" name="cat_title" />
+                                </div>
+                                <div class="form-group">                               
+                                    <input type="submit" class="btn btn-success" name="submit" value="Add Category" />
+                                </div>
+
+                            </form>
+                        </div>
                         
                     </div>
                 </div>
