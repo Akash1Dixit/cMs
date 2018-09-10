46a89808fb57cf807ddbc3c73238e63e6e048c80
Akash Dixit <anshudixit00011@gmail.com>
2018-09-10 20:48:39 +0530

    changes

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,14 +3,17 @@
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
+                    <form action="search.php" method="post">
+                      <div class="input-group">
+                        <input type="text" name="search" class="form-control">
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
                     </div>
+                    </form>
+
                     <!-- /.input-group -->
                 </div>
 
@@ -18,40 +21,31 @@
                 <div class="well">
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
+             <?php 
+                    $query = "SELECT * FROM categories LIMIT 3 ";
+                    $select_all_from_categories = mysqli_query($connection,$query);
+
+               ?>
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+             <?php 
+
+                        while($row= mysqli_fetch_assoc($select_all_from_categories)){
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    } 
+                    ?>
+                                
                             </ul>
                         </div>
                         <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
+                       
                         <!-- /.col-lg-6 -->
                     </div>
                     <!-- /.row -->
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include 'widget.php'; ?>
 
             </div>
