7e4262aa3599316c9f05535d0a48824cedd30b16
Akash Dixit <anshudixit00011@gmail.com>
2018-09-11 21:46:44 +0530

    Adding Posts

--- admin/posts.php
+++ admin/posts.php
@@ -15,53 +15,39 @@
                             Welcome To Admin
                             <small>Author</small>
                         </h1>
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Auhtor</th>
-                                    <th>Title</th>
-                                    <th>Content</th>
-                                    <th>Status</th>
-                                    <th>Images</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Date</th>
-                                </tr>
-                            </thead>
-                            <tbody>
 
-                    <?php 
-                          $query = "SELECT * FROM posts ";
-                          $result = mysqli_query($connection,$query);
-                        while($row= mysqli_fetch_assoc($result)){
-                         $post_id = $row['post_id'];
-                         $post_author = $row['post_author'];
-                         $post_title = $row['post_title'];
-                         $post_content = $row['post_content'];
-                         $post_status = $row['post_status'];
-                         $post_image = $row['post_image'];
-                         $post_tags = $row['post_tags'];
-                         $post_date=$row['post_date'];
+                     <?php 
+                          if(isset($_GET['source'])){
 
-                          echo "<tr>";
-                               echo "<td>$post_id</td>";
-                               echo "<td>$post_author</td>";
-                               echo "<td>$post_title</td>";
-                               echo "<td>$post_content</td>";
-                               echo "<td>$post_status</td>";
-                               echo "<td><img width=100 src='../images/$post_image'></td>";
-                               echo "<td> $post_tags</td>";
-                               echo "<td></td>";
-                               echo "<td> $post_date</td>";
-                           echo "</tr>";
+                            $source = $_GET['source'];
+                          }
 
-                    }
+                          switch ($source) {
+                              case 'add_post':
+                                  echo include "includes/add_post.php";# code...
+                                  break;
+                              case '':
+                                  # code...
+                                  break; 
+                              case '':
+                                  # code...
+                                  break;   
 
-                     ?>
-                                
-                            </tbody>
-                        </table>
+                              
+                              default:
+                                  include "includes/view_all_posts.php"; 
+                                  break;
+                          }
+
+
+
+
+
+                      ?>
+
+
+
+
                     </div>
                 </div>
                 <!-- /.row -->
