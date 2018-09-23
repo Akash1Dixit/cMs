0613a748aef0667b502d2a290c82608df3388e44
Akash Dixit <anshudixit00011@gmail.com>
2018-09-23 20:51:26 +0530

    update

---
+++ admin/comments.php
@@ -0,0 +1,62 @@
+<?php include "includes/adHeader.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/adNavigation.php"; ?>
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                  <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome To Admin
+                            <small>Author</small>
+                        </h1>
+
+                     <?php 
+                          if(isset($_GET['source'])){
+
+                            $source = $_GET['source'];
+                          }else{
+                            $source = '';
+                          }
+
+                          switch ($source) {
+                              case 'add_post':
+                                  echo include "includes/add_post.php";# code...
+                                  break;
+                              case 'edit_post' :
+                                  echo include "includes/edit_post.php";# code...
+                                  break; 
+                              case '45':
+                                 echo "NICE44"; # code...
+                                  break;   
+
+                              
+                              default:
+                                  include "includes/view_all_posts.php"; 
+                                  break;
+                          }
+
+
+
+
+
+                      ?>
+
+
+
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+<?php include "includes/adFooter.php" ; ?>
