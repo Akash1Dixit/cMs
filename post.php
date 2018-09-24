a0d916ca4f51a717cc66231b69056697265859eb
Akash Dixit <anshudixit00011@gmail.com>
2018-09-24 21:50:44 +0530

    update

--- post.php
+++ post.php
@@ -57,14 +57,31 @@
 
                 <!-- Blog Comments -->
 
+                 <?php 
+
+                       if(isset($_POST['comment_btn'])){
+                        echo $_POST['comment_author'];
+                       }
+
+                  ?>
+
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form action="" method="post" role="form">
                         <div class="form-group">
+                            <label for="author">Author</label>
+                            <input type="text" class="form-control" name="comment_author">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="comment_email">
+                        </div>
+                        <div class="form-group">
+                            <label for="content">Content</label>
                             <textarea class="form-control" rows="3"></textarea>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" name="comment_btn" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
